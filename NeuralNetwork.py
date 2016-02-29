9b5e36dc59d12940519ba071162d89b2f43923cf
pbellis <pbellis@bu.edu>
2016-02-29 18:35:19 -0500

    Fixed L2 regularization

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class NeuralNetwork(object):
     
-    def __init__(self, layer_dims, epsilon, activation_function, l2_lambda = 0):
+    def __init__(self, layer_dims, epsilon, activation_function, l2 = 0):
         self.layer_dims = layer_dims
         self.epsilon = epsilon
         self.activation_function = activation_function
@@ -15,7 +15,7 @@ class NeuralNetwork(object):
         self.W = [np.random.randn(self.layer_dims[j], self.layer_dims[j+1]) / np.sqrt(self.layer_dims[j]) for j in xrange(self.output_layer)]
         self.b = [np.zeros((1, layer_dim)) for layer_dim in self.layer_dims[1:]]
         
-        self.l2_lambda = l2_lambda
+        self.l2 = l2
         
     def foward_propagate(self, X):
         activations = [None for layer_dim in self.layer_dims]
@@ -44,10 +44,8 @@ class NeuralNetwork(object):
                         
         for k in xrange(self.output_layer, self.input_layer, -1):
             beta[k-1] = (beta[k].dot(self.W[k-1].T) * self.activation_function(activations[k-1], True))
-            
-            l2 = self.l2_lambda * np.sqrt( np.sum( np.square(self.W[k-1] ) ) ) / self.W[k-1].size
-            
-            dW[k-1] = self.epsilon * (activations[k-1].T).dot(beta[k]) + l2 * self.W[k-1]
+                        
+            dW[k-1] = self.epsilon * (activations[k-1].T).dot(beta[k])
             db[k-1] = self.epsilon * np.sum(beta[k], axis=0, keepdims=True)
             
         return dW, db
@@ -55,13 +53,16 @@ class NeuralNetwork(object):
     def predict(self, X):
         return np.argmax(self.foward_propagate(X)[self.output_layer], axis=1)
         
-    def fit(self,X,y,num_epochs):        
+    def fit(self,X,y,num_epochs):   
+        
+        n = len(y)
+                  
         for epoch in xrange(num_epochs):
             activations = self.foward_propagate(X)
             dW, db = self.back_propagate(X, y, activations)
-            
+                        
             for l in xrange(self.output_layer):
-                self.W[l] -= dW[l]
+                self.W[l] = (1 - (self.l2 * self.epsilon / n) ) * self.W[l] - dW[l]
                 self.b[l] -= db[l]
                 
     def compute_cost(self, X, y):
@@ -70,5 +71,11 @@ class NeuralNetwork(object):
         softmax_scores = self.foward_propagate(X)[self.output_layer]
         cross_ent_err = -np.log(softmax_scores[range(num_samples), y])
         data_loss = np.sum(cross_ent_err)
-        return 1./num_samples * data_loss
         
+        w_sqr = 0.0
+        for layer in self.W:
+            for neuron in layer:
+                for weight in neuron:
+                    w_sqr += weight * weight
+        
+        return 1./num_samples * data_loss * (w_sqr * self.l2  / (2*num_samples))
